20a331854181146176229f7a8dd7e31564b9c0ca
DarthTorus <darth.torus@gmail.com>
2021-01-01 13:15:24 -0500

    Minor Formatting

--- events/message.js
+++ events/message.js
@@ -2,31 +2,31 @@ const client = process.Discord
 module.exports = (client) => {
 
   return ( message ) => {
-      var serverName;
-      var serverID;
-      var channelName;
-      var channelID;
+      var serverName
+      var serverID
+      var channelName
+      var channelID
       //console.log(message);
       if(message.channel.type ==='text') {
-        serverName = message.guild.name;
-        serverID = message.guild.id;
-        channelName = message.channel.name;
+        serverName = message.guild.name
+        serverID = message.guild.id
+        channelName = message.channel.name
         channelID = message.channel.id
       } else {
-        serverName = "Direct Messages";
-        serverID = message.channel.id;
-        channelName = message.author.username;
-        channelID = message.author.id;
+        serverName = "Direct Messages"
+        serverID = message.channel.id
+        channelName = message.author.username
+        channelID = message.author.id
       }
 
-      var userName = message.author.username;
-      var userID = message.author.id;
+      var userName = message.author.username
+      var userID = message.author.id
       var logDate = new Date().toLocaleTimeString('en-US', { hour12: false })
 
       if ( message.author.bot || !message.content.startsWith( process.env.TRIGGER ) ) return
 
       const args = message.content.split(' ')
-      let request = args.shift().slice( process.env.TRIGGER.length );
+      let request = args.shift().slice( process.env.TRIGGER.length )
       let parent;
       if ( client.commands.has( request ) && client.commands.get( request ).has('default') ) {
           parent = request
@@ -39,12 +39,12 @@ module.exports = (client) => {
 
 
       // Log the command
-			console.log(client.colors.brightYellow("Server ID: ") + client.colors.magenta(serverID));
-			console.log(client.colors.brightYellow("Channel ID: ") + client.colors.magenta(channelID));
-			console.log(client.colors.brightYellow("Message ID: ") + client.colors.magenta(message.id));
-			console.log(client.colors.brightCyan(`${logDate} ${userName} - ID: `) + client.colors.brightYellow(`@${userID}`));
-			console.log("in " + client.colors.magenta(serverName + " - #" + channelName));
-      console.log(client.colors.white(message.content));
+      console.log(client.colors.brightYellow("Server ID: ") + client.colors.magenta(serverID))
+      console.log(client.colors.brightYellow("Channel ID: ") + client.colors.magenta(channelID))
+			console.log(client.colors.brightYellow("Message ID: ") + client.colors.magenta(message.id))
+			console.log(client.colors.brightCyan(`${logDate} ${userName} - ID: `) + client.colors.brightYellow(`@${userID}`))
+			console.log("in " + client.colors.magenta(serverName + " - #" + channelName))
+      console.log(client.colors.white(message.content))
 
       if ( !command ) {
         console.log(client.colors.brightRed("Command not found"))
